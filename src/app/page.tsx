21ddb86bccb7c6da5333f7000d04bd38a6dfc4b5
elizabeth-trotter <e.trotter120@gmail.com>
2024-06-24 13:40:47 -0700

    styling + accessibility color updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <ToastContainer />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="w-full max-w-xl bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="text-lg font-bold text-center pb-1">Next.js Form Practice</h1>
+        <p className="text-xs tracking-wide text-center pb-4">Name: Elizabeth Trotter <span className="hidden sm:inline">|</span> <span className="block sm:inline">Updated: June 24, 2024</span></p>
+
         <label htmlFor="first">First Name</label>
         <input type="text" id="first" name="first" value={firstName} onChange={(e) => setFirstName(e.target.value)} minLength={2} maxLength={100} required />
 
@@ -44,16 +47,16 @@ export default function Home() {
         <input type="text" id="last" name="last" value={lastName} onChange={(e) => setLastName(e.target.value)} minLength={2} maxLength={100} required />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="email" autoComplete="true" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
         <label htmlFor="birthdate">Date of Birth</label>
         <input type="date" id="birthdate" name="birthdate" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} max={new Date().toISOString().split("T")[0]} required />
 
         <label htmlFor="address">Address</label>
-        <input type="text" id="address" name="address" value={address} onChange={(e) => setAddress(e.target.value)} maxLength={100} />
+        <input type="text" autoComplete="true" id="address" name="address" value={address} onChange={(e) => setAddress(e.target.value)} maxLength={100} />
 
         <label htmlFor="phone">Phone Number</label>
-        <InputMask mask="(999)-999-9999" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} id="phone" name="phone"></InputMask>
+        <InputMask autoComplete="true" mask="(999)-999-9999" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} id="phone" name="phone"></InputMask>
 
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" value={password}
@@ -69,13 +72,13 @@ export default function Home() {
         <label htmlFor="passwordconfirmed">Confirm Password</label>
         <input type="password" id="passwordconfirmed" name="passwordconfirmed" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
 
-        <h2 className="font-bold py-2">Password Requirements:</h2>
+        <h2 className="font-bold pb-2">Password Requirements:</h2>
         <ol className="text-xs pb-5">
-          <li style={{ color: /.{15,}/.test(password) ? 'green' : 'red' }}>- Be at least 15 characters long</li>
-          <li style={{ color: /[A-Z]/.test(password) ? 'green' : 'red' }}>- Contain 1 uppercase letter</li>
-          <li style={{ color: /[0-9]/.test(password) ? 'green' : 'red' }}>- Contain 1 number</li>
-          <li style={{ color: /[?!@#$%^&*]/.test(password) ? 'green' : 'red' }}>- Contain 1 special character: ? ! @ # $ % ^ & *</li>
-          <li style={{ color: password === confirmPassword && password.length > 0 ? 'green' : 'red' }}>- Confirm password matches</li>
+          <li style={{ color: /.{15,}/.test(password) ? '#008A00' : '#EF0000' }}>- Be at least 15 characters long</li>
+          <li style={{ color: /[A-Z]/.test(password) ? '#008A00' : '#EF0000' }}>- Contain 1 uppercase letter</li>
+          <li style={{ color: /[0-9]/.test(password) ? '#008A00' : '#EF0000' }}>- Contain 1 number</li>
+          <li style={{ color: /[?!@#$%^&*]/.test(password) ? '#008A00' : '#EF0000' }}>- Contain 1 special character: ? ! @ # $ % ^ & *</li>
+          <li style={{ color: password === confirmPassword && password.length > 0 ? '#008A00' : '#EF0000' }}>- Confirm password matches</li>
         </ol>
 
         <button type="submit">Submit</button>
