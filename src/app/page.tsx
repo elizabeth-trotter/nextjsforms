e202e2a6b238ec46479171ca40ee73c0d1eb88d2
elizabeth-trotter <e.trotter120@gmail.com>
2024-06-24 11:53:54 -0700

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import InputMask from 'react-input-mask';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
