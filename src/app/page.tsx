21ecd4e0662cb7b48d8e7a0a11999d88250af864
elizabeth-trotter <e.trotter120@gmail.com>
2024-06-25 14:32:57 -0700

    refactor remove req tooltip + use border red, form data object, error toasts add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,57 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [birthdate, setBirthdate] = useState('');
-  const [address, setAddress] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    birthdate: '',
+    address: '',
+    phoneNumber: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault(); // Prevents default form submission
-    
-    if (password === confirmPassword) {
-      toast("Form submitted successfully!", { type: "success" }); // Display toast notification
-      // Here can add further logic to handle form submission, e.g., send data to backend
+  const updateForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Pass through current values and update
+    setFormData({
+      ...formData, //get the existing form 
+      [e.target.name]: e.target.value //[] to get property name dynamically
+    })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+
+    const isFilled = formData.firstName && formData.lastName && formData.email && formData.birthdate && formData.password && formData.confirmPassword;
+    const passwordsMatch = formData.password === formData.confirmPassword;
+
+    if (isFilled && passwordsMatch) {
+      toast("Form submitted successfully!", { type: "success" });
 
       // Reset all form fields
-      setFirstName('');
-      setLastName('');
-      setEmail('');
-      setBirthdate('');
-      setAddress('');
-      setPhoneNumber('');
-      setPassword('');
-      setConfirmPassword('');
-    };
-  }
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        birthdate: '',
+        address: '',
+        phoneNumber: '',
+        password: '',
+        confirmPassword: '',
+      });
+      setIsSubmitted(false);
+    } else {
+      if (!isFilled) {
+        toast("Please fill out all required fields.", { type: "error" });
+      }
+      if (!passwordsMatch) {
+        toast("Passwords do not match.", { type: "error" });
+      }
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
@@ -40,45 +64,45 @@ export default function Home() {
         <h1 className="text-lg font-bold text-center pb-1">Next.js Form Practice</h1>
         <p className="text-xs tracking-wide text-center pb-4">Name: Elizabeth Trotter <span className="hidden sm:inline">|</span> <span className="block sm:inline">Updated: June 24, 2024</span></p>
 
-        <label htmlFor="first">First Name *</label>
-        <input type="text" id="first" name="first" value={firstName} onChange={(e) => setFirstName(e.target.value)} minLength={2} maxLength={100} required />
+        <label htmlFor="firstName">First Name *</label>
+        <input type="text" id="firstName" name="firstName" className={`${isSubmitted && formData.firstName === '' ? 'border-red-500' : ''}`} value={formData.firstName} onChange={updateForm} minLength={2} maxLength={100} />
 
-        <label htmlFor="last">Last Name *</label>
-        <input type="text" id="last" name="last" value={lastName} onChange={(e) => setLastName(e.target.value)} minLength={2} maxLength={100} required />
+        <label htmlFor="lastName">Last Name *</label>
+        <input type="text" id="lastName" name="lastName" className={`${isSubmitted && formData.lastName === '' ? 'border-red-500' : ''}`} value={formData.lastName} onChange={updateForm} minLength={2} maxLength={100} />
 
         <label htmlFor="email">Email *</label>
-        <input type="email" autoComplete="true" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="email" autoComplete="email" id="email" name="email" className={`${isSubmitted && formData.email === '' ? 'border-red-500' : ''}`} value={formData.email} onChange={updateForm} />
 
         <label htmlFor="birthdate">Date of Birth *</label>
-        <input type="date" id="birthdate" name="birthdate" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} max={new Date().toISOString().split("T")[0]} required />
+        <input type="date" id="birthdate" name="birthdate" className={`${isSubmitted && formData.birthdate === '' ? 'border-red-500' : ''}`} value={formData.birthdate} onChange={updateForm} max={new Date().toISOString().split("T")[0]} />
 
         <label htmlFor="address">Address</label>
-        <input type="text" autoComplete="true" id="address" name="address" value={address} onChange={(e) => setAddress(e.target.value)} maxLength={100} />
+        <input type="text" autoComplete="street-address" id="address" name="address" value={formData.address} onChange={updateForm} maxLength={100} />
 
-        <label htmlFor="phone">Phone Number</label>
-        <InputMask autoComplete="true" mask="(999)-999-9999" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} id="phone" name="phone"></InputMask>
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <InputMask autoComplete="tel" mask="(999)-999-9999" value={formData.phoneNumber} onChange={updateForm} id="phoneNumber" name="phoneNumber"></InputMask>
 
         <label htmlFor="password">Password *</label>
-        <input type="password" id="password" name="password" value={password}
+        <input type="password" id="password" name="password" className={`${isSubmitted && formData.password === '' ? 'border-red-500' : ''}`} value={formData.password}
           pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[?@!#$%^&*])(?!.*[^A-Za-z\d?@!#$%^&*]).{15,}$"
           onChange={(e) => {
             const { value } = e.target;
             // Remove any characters that are not in the allowed set
             const sanitizedValue = value.replace(/[^A-Za-z\d?@!#$%^&*]/g, '');
-            setPassword(sanitizedValue);
+            setFormData({ ...formData, password: sanitizedValue });
           }}
-          required />
+          />
 
-        <label htmlFor="passwordconfirmed">Confirm Password *</label>
-        <input type="password" id="passwordconfirmed" name="passwordconfirmed" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-        
+        <label htmlFor="confirmPassword">Confirm Password *</label>
+        <input type="password" id="confirmPassword" name="confirmPassword" className={`${isSubmitted && formData.confirmPassword === '' ? 'border-red-500' : ''}`} value={formData.confirmPassword} onChange={updateForm} />
+
         <h2 className="font-bold pb-2">Password Requirements:</h2>
         <ol className="text-xs pb-5">
-          <li style={{ color: /.{15,}/.test(password) ? '#004600' : '#8B0000' }}>- Be at least 15 characters long</li>
-          <li style={{ color: /[A-Z]/.test(password) ? '#004600' : '#8B0000' }}>- Contain 1 uppercase letter</li>
-          <li style={{ color: /[0-9]/.test(password) ? '#004600' : '#8B0000' }}>- Contain 1 number</li>
-          <li style={{ color: /[?!@#$%^&*]/.test(password) ? '#004600' : '#8B0000' }}>- Contain 1 special character: ? ! @ # $ % ^ & *</li>
-          <li style={{ color: password === confirmPassword && password.length > 0 ? '#004600' : '#8B0000' }}>- Confirm password matches</li>
+          <li style={{ color: /.{15,}/.test(formData.password) ? '#004600' : '#8B0000' }}>- Be at least 15 characters long</li>
+          <li style={{ color: /[A-Z]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 uppercase letter</li>
+          <li style={{ color: /[0-9]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 number</li>
+          <li style={{ color: /[?!@#$%^&*]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 special character: ? ! @ # $ % ^ & *</li>
+          <li style={{ color: formData.password === formData.confirmPassword && formData.password.length > 0 ? '#004600' : '#8B0000' }}>- Confirm password matches</li>
         </ol>
 
         <button type="submit">Submit</button>
