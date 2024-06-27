a18c5eeb2e85c7ffa6b870af0005281fc7e5677b
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-06-27 15:49:49 -0700

    style update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,60 +84,100 @@ export default function Home() {
       }
     }
   };
-  
+
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+    <main className="min-h-screen w-full bg-[#23527C] flex items-center justify-center">
       <ToastContainer />
-      <form onSubmit={handleSubmit} className="w-full max-w-xl bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 block ">
-        <h1 className="text-lg font-bold text-center pb-1">Next.js Form Practice</h1>
-        <p className="text-xs tracking-wide text-center pb-4">Name: Elizabeth Trotter <span className="hidden sm:inline">|</span> <span className="block sm:inline">Updated: June 27, 2024</span></p>
+      <div className="flex items-center flex-col">
+        <img src="/WA-Logo.png" alt="William's Act Logo" />
+        <div className="bg-white px-6 py-4 min-w-72 sm:max-w-[538px] max-w-[288px] mb-12">
+          <h1 className="text-center text-[34px] text-black mb-6 robotoCondensed font-light ">USER <strong className="font-bold">SIGN UP</strong></h1>
 
-        <label htmlFor="firstname">First Name *</label>
-        <input type="text" id="firstname" name="firstname" className={`${isSubmitted && formData.firstname === '' ? 'border-red-500' : ''}`} value={formData.firstname} onChange={updateForm} minLength={2} maxLength={100} />
+          <form onSubmit={handleSubmit} className="openSans font-semibold">
+            <div className="grid sm:grid-cols-2 gap-6 ">
 
-        <label htmlFor="lastname">Last Name *</label>
-        <input type="text" id="lastname" name="lastname" className={`${isSubmitted && formData.lastname === '' ? 'border-red-500' : ''}`} value={formData.lastname} onChange={updateForm} minLength={2} maxLength={100} />
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
+                <input placeholder="First Name" type="text" id="firstName" name="firstName" className={`${isSubmitted && formData.firstname === '' ? 'border-red-500 text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12' : 'text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12'}`} value={formData.firstname} onChange={updateForm} minLength={2} maxLength={100} />
+              </div>
 
-        <label htmlFor="email">Email *</label>
-        <input type="email" autoComplete="email" id="email" name="email" className={`${isSubmitted && formData.email === '' ? 'border-red-500' : ''}`} value={formData.email} onChange={updateForm} />
 
-        <label htmlFor="dob">Date of Birth *</label>
-        <input type="date" id="dob" name="dob" className={`${isSubmitted && formData.dob === '' ? 'border-red-500' : ''}`} value={formData.dob} onChange={updateForm} max={maxDate} />
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
+                <input placeholder="Last Name" type="text" id="lastName" name="lastName" className={`${isSubmitted && formData.lastname === '' ? 'border-red-500 text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12' : 'text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12'}`} value={formData.lastname} onChange={updateForm} minLength={2} maxLength={100} />
+              </div>
 
-        <label htmlFor="address">Address</label>
-        <input type="text" autoComplete="street-address" id="address" name="address" value={formData.address} onChange={updateForm} maxLength={100} />
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
+                <input placeholder="Email" type="email" autoComplete="email" id="email" name="email" className={`${isSubmitted && formData.email === '' ? 'border-red-500 text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12' : 'text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12'}`} value={formData.email} onChange={updateForm} />
+              </div>
 
-        <label htmlFor="phonenumber">Phone Number</label>
-        <InputMask autoComplete="tel" mask="(999)-999-9999" value={formData.phonenumber} onChange={updateForm} id="phonenumber" name="phonenumber"></InputMask>
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
 
-        <label htmlFor="password">Password *</label>
-        <input type="password" id="password" name="password" className={`${isSubmitted && formData.password === '' ? 'border-red-500' : ''}`} value={formData.password}
-          pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[?@!#$%^&*])(?!.*[^A-Za-z\d?@!#$%^&*]).{15,}$"
-          onChange={(e) => {
-            const { value } = e.target;
-            // Remove any characters that are not in the allowed set
-            const sanitizedValue = value.replace(/[^A-Za-z\d?@!#$%^&*]/g, '');
-            setFormData({ ...formData, password: sanitizedValue });
-          }}
-        />
+                <input placeholder="Birthdate" type="date" id="birthdate" name="birthdate" className={`${isSubmitted && formData.dob === '' ? 'border-red-500 text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12' : 'text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12'}`} value={formData.dob} onChange={updateForm} max={maxDate} />
+              </div>
 
-        <label htmlFor="confirmPassword">Confirm Password *</label>
-        <input type="password" id="confirmPassword" name="confirmPassword" className={`${isSubmitted && formData.confirmPassword === '' ? 'border-red-500' : ''}`} value={formData.confirmPassword} onChange={updateForm} />
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
 
-        <h2 className="font-bold pb-2">Password Requirements:</h2>
-        <ol className="text-xs pb-5">
-          <li style={{ color: /.{15,}/.test(formData.password) ? '#004600' : '#8B0000' }}>- Be at least 15 characters long</li>
-          <li style={{ color: /[A-Z]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 uppercase letter</li>
-          <li style={{ color: /[0-9]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 number</li>
-          <li style={{ color: /[?!@#$%^&*]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 special character: ? ! @ # $ % ^ & *</li>
-          <li style={{ color: formData.password === formData.confirmPassword && formData.password.length > 0 ? '#004600' : '#8B0000' }}>- Confirm password matches</li>
-        </ol>
+                <input className="text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12" placeholder="Address" type="text" autoComplete="street-address" id="address" name="address" value={formData.address} onChange={updateForm} maxLength={100} />
+              </div>
 
-        <button type="submit">Submit</button>
-        <p className="text-xs text-end pt-1">* fields required</p>
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
 
-      </form>
+                <InputMask className="text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12" placeholder="(xxx)-xxx-xxxx" autoComplete="tel" mask="(999)-999-9999" value={formData.phonenumber} onChange={updateForm} id="phoneNumber" name="phoneNumber"></InputMask>
+              </div>
+
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
+                <input placeholder="Password" type="password" id="password" name="password" className={`${isSubmitted && formData.password === '' ? 'border-red-500 text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12' : 'text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12'}`} value={formData.password}
+                  pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[?@!#$%^&*])(?!.*[^A-Za-z\d?@!#$%^&*]).{15,}$"
+                  onChange={(e) => {
+                    const { value } = e.target;
+                    // Remove any characters that are not in the allowed set
+                    const sanitizedValue = value.replace(/[^A-Za-z\d?@!#$%^&*]/g, '');
+                    setFormData({ ...formData, password: sanitizedValue });
+                  }}
+                />
+              </div>
+
+
+
+              <div className='flex flex-col relative'>
+                <p className='text-red-600 absolute top-0 right-1'>*</p>
+                <input placeholder="Re-Type Password" type="password" id="confirmPassword" name="confirmPassword" className={`${isSubmitted && formData.confirmPassword === '' ? 'border-red-500 text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12' : 'text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12'}`} value={formData.confirmPassword} onChange={updateForm} />
+              </div>
+
+
+              <div className=" col-span-2">
+                <h2 className="font-bold pb-2">Password Requirements:</h2>
+                <ol className="text-xs pb-5">
+                  <li style={{ color: /.{15,}/.test(formData.password) ? '#004600' : '#8B0000' }}>- Be at least 15 characters long</li>
+                  <li style={{ color: /[A-Z]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 uppercase letter</li>
+                  <li style={{ color: /[0-9]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 number</li>
+                  <li style={{ color: /[?!@#$%^&*]/.test(formData.password) ? '#004600' : '#8B0000' }}>- Contain 1 special character: ? ! @ # $ % ^ & *</li>
+                  <li style={{ color: formData.password === formData.confirmPassword && formData.password.length > 0 ? '#004600' : '#8B0000' }}>- Confirm password matches</li>
+                </ol>
+
+              </div>
+
+
+            </div>
+
+            <div className="flex justify-center mt-6 w-full flex-col">
+              <button type="button" className="bg-[#DD8A3E] hover:brightness-90 p-4 w-full text-white text-sm font-bold tracking-wide" >Submit</button>
+              <p className="text-xs text-end pt-1">* fields required</p>
+            </div>
+
+
+          </form>
+        </div>
+      </div>
+
+
     </main>
   );
 };
