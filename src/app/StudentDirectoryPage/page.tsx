8ceba24d8403395ebd39253531b0ca719d07eb70
Jeriah Marson <jeriahmarson@gmail.com>
2024-06-28 11:37:20 -0700

    very small stuff

--- src/app/StudentDirectoryPage/page.tsx
+++ src/app/StudentDirectoryPage/page.tsx
@@ -1,9 +1,30 @@
+import FooterComponent from '@/components/FooterComponent/page'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col'>
+      <table>
+        <thead>
+            <th className=''> IMAGE</th>
+            <td>Name</td>
+            <td>Date of Birth</td>
+            <td>Address</td>
+            <td>Email</td>
+            <td>Phone</td>
+        </thead>
+        <tbody>
+          <tr>
+            <th className=''></th>
+            <td>Marcos Rodriguez</td>
+            <td>9/11/2001</td>
+            <td>2922 Transworld Dr, Stockton, CA 95206</td>
+            <td>marcos.e.rodriguez2003@gmail.com</td>
+            <td>911</td>
+          </tr>
+        </tbody>
+      </table>
+      <FooterComponent/>
     </div>
   )
 }
