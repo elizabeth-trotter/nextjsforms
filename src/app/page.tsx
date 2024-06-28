1e172d24d1432c5f4252f9f47c742b015326c744
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-06-28 08:25:19 -0700

    fixing error text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
+
+
 import PasswordRulesComponent from "@/components/PasswordRulesComponent";
 import { GetFormsAPI, SendFormAPI } from "@/utils/DataServices/DataService";
 import { useEffect, useState } from "react";
 import InputMask from 'react-input-mask';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { IoLogoFacebook } from "react-icons/io";
 
 export default function Home() {
   const [formData, setFormData] = useState({
@@ -108,15 +111,15 @@ export default function Home() {
 
 
   return (
-    <main className="min-h-screen w-full bg-[#23527C] flex items-center justify-center">
+    <main className=" min-h-screen w-full bg-[#23527C] flex items-center justify-center">
       <ToastContainer />
       <div className="flex items-center flex-col">
-        <img className="w-[230px] mb-4" src="/WA-Logo.png" alt="William's Act Logo" />
+        <img className="w-[230px] p-5 mb-4" src="/WA-Logo.png" alt="William's Act Logo" />
         <div className="bg-white px-6 py-4 sm:min-w-[538px] sm:max-w-[538px] max-w-[288px] mb-12">
           <h1 className="text-center text-[34px] text-black mb-6 robotoCondensed font-light ">USER <strong className="font-bold">SIGN UP</strong></h1>
 
           <form onSubmit={handleSubmit} className="openSans font-semibold">
-            <div className="grid sm:grid-cols-2 gap-6 ">
+            <div className="grid sm:grid-cols-2 gap-x-6 gap-y-4 ">
 
               <div className='flex flex-col relative'>
                 <p className='text-red-600 absolute top-0 right-1'>*</p>
@@ -154,21 +157,21 @@ export default function Home() {
               </div>
 
               <div className='flex flex-col relative'>
-                <p className='text-red-600 absolute top-0 right-1'>*</p>
+
 
                 <input className="text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12" placeholder="Address" type="text" autoComplete="street-address" id="address" name="address" value={formData.address} onChange={updateForm} maxLength={100} />
               </div>
 
               <div className='flex flex-col relative'>
-                <p className='text-red-600 absolute top-0 right-1'>*</p>
+
 
                 <InputMask className="text-center bg-[#ECF0F1] p-4 text-sm text-black mb-4 focus:outline-[#DD8A3E] focus:rounded-none h-12" placeholder="(xxx)-xxx-xxxx" autoComplete="tel" mask="(999)-999-9999" value={formData.phonenumber} onChange={updateForm} id="phonenumber" name="phonenumber"></InputMask>
               </div>
 
               <div className='flex flex-col relative'>
-                
+
                 {
-                 showPasswordToolTip && <div className="absolute -top-44 bg-white w-80 rounded-sm openSans mx-auto left-0 right-0 shadow-md p-4 z-40">
+                  showPasswordToolTip && <div className="absolute -top-44 bg-white w-80 rounded-sm openSans mx-auto left-0 right-0 shadow-md p-4 z-40">
                     <h1 className="openSans font-semibold mb-2">Password Requirements</h1>
                     <PasswordRulesComponent correct={/.{15,}/.test(formData.password)} error={formData.password === "" ? false : true} rule="Be at least 15 characters long" />
                     <PasswordRulesComponent correct={/[A-Z]/.test(formData.password)} error={/[A-Za-z]/.test(formData.password) ? true : false} rule="Contain 1 uppercase letter" />
@@ -215,9 +218,8 @@ export default function Home() {
 
             <div className="flex justify-center mt-6 w-full flex-col">
               <button type="submit" className="bg-[#DD8A3E] hover:brightness-90 p-4 w-full text-white text-sm font-bold tracking-wide" >Submit</button>
-              <p className="text-xs text-end pt-1 openSans">* fields required</p>
+              <p className=" text-red-600 text-xs text-end pt-1 openSans">* fields required</p>
             </div>
-
 
           </form>
         </div>
