6957950f28939145a21c7cd1951c7ca5412ba357
JayvonK <jaykhuth@gmail.com>
2024-07-01 10:37:38 -0700

    camelCase on IForm

--- src/Interfaces/Interface.ts
+++ src/Interfaces/Interface.ts
@@ -6,8 +6,8 @@ export interface ICreateAccount {
 }
 
 export interface IForm {
-    firstname: string;
-    lastname: string;
+    firstName: string;
+    lastName: string;
     address: string;
     phonenumber: string;
     email: string;
