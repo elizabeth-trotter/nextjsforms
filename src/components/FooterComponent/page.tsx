4026a6910514ce76162ac2d99219a202b3f3c127
jerrodmpackard <jerrodmpackard@gmail.com>
2024-07-02 10:57:19 -0700

    Footer responsiveness and icon size adjustments

--- src/components/FooterComponent/page.tsx
+++ src/components/FooterComponent/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const FooterComponent = () => {
     return (
         <div className="bg-[#737375]  py-[30px] ">
-            <div className=" grid grid-cols-1 md:grid-cols-3 mx-auto  max-w-[1200px]">
+            <div className=" grid grid-cols-1 md:grid-cols-3 mx-auto md:max-w-[760px] lg:max-w-[970px] xl:max-w-[1200px]">
                 <div className="text-white py-[20px] px-[15px]">
                     <p className=" mb-[20px]  robotoCondensed text-[22px]">WilliamsAct.org</p>
                     <div className="mb-[20px]">
@@ -24,18 +24,18 @@ const FooterComponent = () => {
                             <a href="https://www.instagram.com/CodeStackSJ"><Image width={30} height={30} className="rounded-md" alt="Instagram" src="/InstagramLogo.png" /></a>
                             <a href="https://www.youtube.com/CodeStackSJ"><Image width={30} height={30} className="rounded-md" alt="YouTube" src="/YouTubeLogo.png" /></a>
                         </div>
-                        <a href="https://www.codestack.org/" target='_blank'><Image width={150} height={50} className="my-[5px]" src="/CodeStackFooter.png" alt="CodeStack Logo" /></a>
-                        <a href="https://www.sjcoe.org/" target='_blank'><Image width={150} height={50} className=" my-[5px]" src="/SjcoeLogo.png" alt="SJCOE Logo" /></a>
+                        <a href="https://www.codestack.org/" target='_blank'><Image width={150} height={43} className="my-[5px]" src="/CodeStackFooter.png" alt="CodeStack Logo" /></a>
+                        <a href="https://www.sjcoe.org/" target='_blank'><Image width={150} height={42} className=" my-[5px]" src="/SjcoeLogo.png" alt="SJCOE Logo" /></a>
                     </div>
                 </div>
 
                 <div className="  py-[20px] text-white px-[15px]">
                     <p className=" mb-[20px] robotoCondensed text-[22px]">CodeStack Sites</p>
-                    <a href="https://codestack.org/" target='_blank' className='flex flex-row hover:underline'><Image width={22} height={22} className='mr-[10px]' src="/icon_codestack.png" alt="CodeStack Logo" />CodeStack</a>
-                    <a href="https://seis.org/" target='_blank' className='flex flex-row hover:underline'><Image width={22} height={22} className='mr-[10px]' src="/icon_seis.png" alt="SEIS Logo" />SEIS</a>
-                    <a href="https://edjoin.org/" target='_blank' className='flex flex-row hover:underline'><Image width={22} height={22} className='mr-[10px]' src="/icon_edjoin.png" alt="EDJOIN Logo" />EDJOIN</a>
-                    <a href="https://mypromis.org/" target='_blank' className='flex flex-row hover:underline'><Image width={22} height={22} className='mr-[10px]' src="/icon_promis.png" alt="PROMIS Logo" />PROMIS</a>
-                    <a href="https://sstonline.org/" target='_blank' className='flex flex-row hover:underline'><Image width={22} height={22} className='mr-[10px]' src="/icon_sst.png" alt="Beyond SST Logo" />Beyond SST</a>
+                    <a href="https://codestack.org/" target='_blank' className='flex flex-row hover:underline'><Image width={24} height={24} className='mr-[10px]' src="/icon_codestack.png" alt="CodeStack Logo" />CodeStack</a>
+                    <a href="https://seis.org/" target='_blank' className='flex flex-row hover:underline'><Image width={24} height={24} className='mr-[10px]' src="/icon_seis.png" alt="SEIS Logo" />SEIS</a>
+                    <a href="https://edjoin.org/" target='_blank' className='flex flex-row hover:underline'><Image width={24} height={24} className='mr-[10px]' src="/icon_edjoin.png" alt="EDJOIN Logo" />EDJOIN</a>
+                    <a href="https://mypromis.org/" target='_blank' className='flex flex-row hover:underline'><Image width={24} height={24} className='mr-[10px]' src="/icon_promis.png" alt="PROMIS Logo" />PROMIS</a>
+                    <a href="https://sstonline.org/" target='_blank' className='flex flex-row hover:underline'><Image width={24} height={24} className='mr-[10px]' src="/icon_sst.png" alt="Beyond SST Logo" />Beyond SST</a>
                 </div>
             </div>
         </div>
